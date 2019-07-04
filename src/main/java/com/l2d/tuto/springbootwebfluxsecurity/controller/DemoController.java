fec5a30291936d8f862d0afab489f22e7426e2f1
Duy Le <ldduy1006@gmail.com>
2019-07-04 23:27:55 +0200

    Github #1 : Get username from rest api controller

--- src/main/java/com/l2d/tuto/springbootwebfluxsecurity/controller/DemoController.java
+++ src/main/java/com/l2d/tuto/springbootwebfluxsecurity/controller/DemoController.java
@@ -1,9 +1,11 @@
 package com.l2d.tuto.springbootwebfluxsecurity.controller;
 
+import com.l2d.tuto.springbootwebfluxsecurity.security.SecurityUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 /**
@@ -13,7 +15,8 @@ import reactor.core.publisher.Mono;
 @RequestMapping(value = "/api")
 public class DemoController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public Mono<String> hello(@RequestParam String name) {
-        return Mono.just("Hello " + name);
+    public Mono<String> hello(ServerWebExchange serverWebExchange,  @RequestParam String name) {
+
+       return SecurityUtils.getUserFromRequest(serverWebExchange);
     }
 }
